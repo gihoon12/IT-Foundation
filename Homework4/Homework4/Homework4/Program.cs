b70418c992ead0340a1ca1d485f58cccae14e17d
gihoon12 <gihoon12@naver.com>
2016-07-14 18:30:13 +0900

    Homework4 done!

---
+++ Homework4/Homework4/Homework4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            String strnum = Console.ReadLine();
+            int num = int.Parse(strnum);
+            int x = 1;
+            if (num > 0)
+            {
+                while (x <= num)
+                {
+                    int value = 1;
+                    for (int y = x; y > 0; y--)
+                    {
+                        value = value * y;
+                    }
+                    Console.WriteLine("{0}!  =   {1}", x, value);
+                    x++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("0!  = 0");
+            }
+            Console.ReadLine();
+        }
+    }
+}
